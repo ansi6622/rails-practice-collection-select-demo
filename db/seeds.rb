6c3cf944714ef99945c3d62fd223fa13f5d45737
Jeff Dean <jeff@galvanize.it>
2014-11-17 15:00:51 -0800

    prepped the collection select demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Cat.destroy_all
 Toy.destroy_all
 
-10.times do
+4.times do
   cat = Cat.create!(name: Faker::Name.first_name)
-  rand(6).times do
+  rand(3).times do
     Toy.create!(
       cat_id: cat.id,
       name: Faker::Commerce.product_name
